651170e2c5858170275858d7798f3258db0ace49
sin-sky-sora <yukiakisinsora00@yahoo.co.jp>
2020-06-07 00:51:13 +0900

    モデルの作成

--- image_project/judge/models.py
+++ image_project/judge/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+LearnChoise = ((1,"VGG16"),(2,"VGG19"))
+class ImageModel(models.Model):
+    images = models.ImageField(upload_to='images',null=True, blank=True)
+    ##画像のアップロード場所
+    user = models.UUIDField(editable=False)
+    ##ユーザー識別用のUUID      COOKIEに保存
+    release = models.BooleanField(default=False)
+    ##公開or非公開
+    learning = models.BooleanField(default=False)
+    ##学習に協力するか否か
+    answer = models.CharField(max_length=255,null=True,blank=True)
+    ##答え（学習に協力する場合入力を求める）
+    title = models.CharField(max_length=255)
+    ##表示するタイトル
+    learn_model = models.IntegerField()
+    ##使用するモデルの選択
+
+class LearningModel(models.Model):
+    image_pk = models.IntegerField()
+    ##画像のpkと結びつけ
+    en = models.CharField(max_length=255)
+    ##英語
+    jp = models.CharField(max_length=255)
+    ##日本語訳
+    tie = models.FloatField()
+    ##確率
