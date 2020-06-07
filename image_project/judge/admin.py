52141196bad4c56461e2df4f0888a642886e6c0b
sin-sky-sora <yukiakisinsora00@yahoo.co.jp>
2020-06-07 16:08:41 +0900

    管理者サイトでのmodelの可視化

--- image_project/judge/admin.py
+++ image_project/judge/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import ImageModel, LearningModel
 # Register your models here.
+
+admin.site.register(ImageModel)
+admin.site.register(LearningModel)
