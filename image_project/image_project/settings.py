ef9c3e6a51050486d3760f9700f0a61304f7cbf3
sin-sky-sora <yukiakisinsora00@yahoo.co.jp>
2020-07-02 00:00:43 +0900

    heroku sub

--- image_project/image_project/settings.py
+++ image_project/image_project/settings.py
@@ -23,7 +23,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ux!=e62=p)w407v-_t5%&&@$s=tv#b$af#yd6$!mx122orfzs5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
+if not DEBUG:
+    import django_heroku
+    django_heroku.settings(locals())
 
 ALLOWED_HOSTS = ["*"]
 
